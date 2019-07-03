9a0672f3e3c656f03739cd8cb78a5a06dfaf3e67
Appu Goundan <appu@google.com>
2019-07-03 15:27:59 -0400

    Move to unified gradle build
    - also upgrades gradle to 5.2.1
    - all builds should happen from root :<project>:<task>
    - install task is replaced by publishToMavenLocal for local testing

--- jib-maven-plugin/src/integration-test/java/com/google/cloud/tools/jib/maven/BuildTarMojoIntegrationTest.java
+++ jib-maven-plugin/src/integration-test/java/com/google/cloud/tools/jib/maven/BuildTarMojoIntegrationTest.java
@@ -30,13 +30,9 @@ import org.junit.Test;
 
 public class BuildTarMojoIntegrationTest {
 
-  @ClassRule public static final TestPlugin testPlugin = new TestPlugin();
+  @ClassRule public static final TestProject simpleTestProject = new TestProject("simple");
 
-  @ClassRule
-  public static final TestProject simpleTestProject = new TestProject(testPlugin, "simple");
-
-  @ClassRule
-  public static final TestProject skippedTestProject = new TestProject(testPlugin, "empty");
+  @ClassRule public static final TestProject skippedTestProject = new TestProject("empty");
 
   /**
    * Builds and runs jib:buildTar on a project at {@code projectRoot} pushing to {@code
